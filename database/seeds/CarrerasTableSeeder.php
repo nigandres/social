f4a86243bfc0e9c6b69b3fa10c4648ae99487dba
Unknown <andressolis1@hotmail.es>
2017-06-27 16:29:55 -0500

    seeders algoritmos

--- database/seeds/CarrerasTableSeeder.php
+++ database/seeds/CarrerasTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CarrerasTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $this->call(CarrerasTableSeeder::class);/// le puse Carreras
+        //
+        DB::table('carreras')->insert([
+            'carrera' => str_random(10),
+        ]);
     }
 }
